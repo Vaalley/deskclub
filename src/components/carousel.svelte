dc90130a2326693bffab11b0350576a072e46817
Vaalley <vaaleyou@gmail.com>
2022-07-29 06:54:10 +0200

    Added carousel

---
+++ src/components/carousel.svelte
@@ -0,0 +1,40 @@
+<script>
+	// Import Swiper Svelte components
+	import { Swiper, SwiperSlide } from 'swiper/svelte';
+
+	// Import Swiper styles
+	import 'swiper/css';
+</script>
+
+<Swiper
+	spaceBetween={50}
+	slidesPerView={1}
+	navigation
+	pagination
+	loop
+	on:slideChange={() => console.log('slide change')}
+	on:swiper={(e) => console.log(e.detail[0])}
+>
+	<SwiperSlide
+		><img
+			src="../../static/images/office-1.webp"
+			alt="Office"
+			class="w-full my-[-15vh]"
+		/></SwiperSlide
+	>
+	<SwiperSlide
+		><img
+			src="../../static/images/office-2.webp"
+			alt="Office"
+			class="w-full my-[-15vh]"
+		/></SwiperSlide
+	>
+	<SwiperSlide
+		><img
+			src="../../static/images/office-3.webp"
+			alt="Office"
+			class="w-full my-[-15vh]"
+		/></SwiperSlide
+	>
+	...
+</Swiper>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Navbar from '../components/navbar.svelte';
 	import Hero from '../components/hero.svelte';
+	import Carousel from '../components/carousel.svelte';
 </script>
 
 <svelte:head>
@@ -10,4 +11,5 @@
 <template>
 	<Navbar />
 	<Hero />
+	<Carousel />
 </template>
